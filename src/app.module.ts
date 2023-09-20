7217523fa32f85330b715a243aaf82cc7a56d504
Ali Madooei <alimadooei@gmail.com>
2023-09-19 22:48:57 -0400

    Set up database integration with TypeORM and Docker
    - Added Docker configuration for PostgreSQL.
    - Installed necessary NestJS modules for TypeORM and configuration handling.
    - Configured AppModule to connect to the database using environment variables.

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,27 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoursesController } from './courses/courses.controller';
 import { CoursesService } from './courses/courses.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(), // Load the .env file
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USER'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: configService.get<string>('NODE_ENV') !== 'production',
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController, CoursesController],
   providers: [AppService, CoursesService],
 })

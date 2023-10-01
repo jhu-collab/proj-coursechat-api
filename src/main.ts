fe71a3645eff60eb8422d6e99d13ceac81c30681
Ali Madooei <alimadooei@gmail.com>
2023-10-01 00:26:01 -0400

    Install helmet

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,14 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { HttpResponseFilter } from './filters/http-response.filter';
 import { HttpResponseInterceptor } from './interceptors/http-response.interceptor';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new HttpResponseFilter());
   app.useGlobalInterceptors(new HttpResponseInterceptor());
+  app.use(helmet());
   await app.listen(3000);
 }
 bootstrap();

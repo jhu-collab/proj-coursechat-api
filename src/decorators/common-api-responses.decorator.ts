2e8d5d99ea16a488275a270fba9887cba0a3da1b
Ali Madooei <alimadooei@gmail.com>
2024-01-04 10:13:41 -0500

    Add documentation to custom decorators

--- src/decorators/common-api-responses.decorator.ts
+++ src/decorators/common-api-responses.decorator.ts
@@ -2,6 +2,26 @@ import { applyDecorators } from '@nestjs/common';
 import { ApiResponse } from '@nestjs/swagger';
 import { ErrorResponseDTO } from 'src/dto/error-response.dto';
 
+/**
+ * Custom decorator that applies common API response annotations for Swagger documentation.
+ *
+ * This decorator adds standardized response information for common HTTP status codes,
+ * such as 400, 401, 404, and 500. It utilizes the `ErrorResponseDTO` to describe the shape
+ * of the error response for these statuses. It's a convenient way to ensure consistent
+ * documentation across different parts of the application.
+ *
+ * Usage:
+ * Apply this decorator to controller methods to automatically document common error responses.
+ *
+ * @example
+ * ```
+ * @Get('/some-endpoint')
+ * @CommonApiResponses()
+ * someMethod() {
+ *   // Controller logic...
+ * }
+ * ```
+ */
 export function CommonApiResponses() {
   return applyDecorators(
     ApiResponse({
--- src/decorators/roles.decorator.ts
+++ src/decorators/roles.decorator.ts
@@ -2,4 +2,29 @@ import { SetMetadata } from '@nestjs/common';
 import { ApiKeyRoles } from 'src/api-key/api-key-roles.enum';
 
 export const ROLES_KEY = 'roles';
+
+/**
+ * Custom decorator for assigning roles to route handlers.
+ *
+ * This decorator uses NestJS's `SetMetadata` to associate the specified roles with a route.
+ * It can be used for role-based access control, in conjunction with guards that check these roles.
+ *
+ * The roles are defined in the `ApiKeyRoles` enum, and this decorator allows specifying one or more roles
+ * that are required to access the route.
+ *
+ * Usage:
+ * Apply this decorator to controller methods to restrict access based on user roles.
+ *
+ * @example
+ * ```
+ * @Get('/protected-route')
+ * @Roles(ApiKeyRoles.ADMIN, ApiKeyRoles.USER)
+ * handleProtectedRoute() {
+ *   // Controller logic for a route accessible only to users with ADMIN or USER roles...
+ * }
+ * ```
+ *
+ * @param {...ApiKeyRoles[]} roles - The roles required to access the route.
+ * @returns {DecoratorFunction} - A custom decorator function for setting metadata on the route.
+ */
 export const Roles = (...roles: ApiKeyRoles[]) => SetMetadata(ROLES_KEY, roles);

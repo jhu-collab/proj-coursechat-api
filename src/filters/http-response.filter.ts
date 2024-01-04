3f5b65107d383386c46d7f15b01127d5647f121f
Ali Madooei <alimadooei@gmail.com>
2024-01-04 10:13:51 -0500

    Add documentation and log messages

--- src/filters/http-response.filter.ts
+++ src/filters/http-response.filter.ts
@@ -1,16 +1,33 @@
 import { Catch, ArgumentsHost, HttpException, Logger } from '@nestjs/common';
-import { Response } from 'express';
+import { Response, Request } from 'express';
 
-const logger = new Logger('HttpResponseFilter');
-
+/**
+ * A global exception filter that formats and handles HTTP exceptions and other types of exceptions.
+ *
+ * This filter catches exceptions thrown from within the application and formats them into a standardized JSON structure.
+ * It handles both NestJS's HttpException (which includes standard HTTP exceptions like BadRequestException)
+ * and generic exceptions, ensuring a consistent response structure for all errors.
+ *
+ * @Catch() - Indicates that this filter is meant to catch exceptions.
+ */
 @Catch()
 export class HttpResponseFilter {
+  private readonly logger = new Logger(HttpResponseFilter.name);
+
+  /**
+   * Method to handle caught exceptions and format the response.
+   *
+   * @param {unknown} exception - The caught exception.
+   * @param {ArgumentsHost} host - Contextual information about the host (HTTP-specific in this case).
+   */
   catch(exception: unknown, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
     const request = ctx.getRequest<Request>();
     const status =
       exception instanceof HttpException ? exception.getStatus() : 500;
+
+    this.logger.error(`Exception caught: ${exception}`);
 
     let errorResponse = {
       statusCode: status,
@@ -22,19 +39,24 @@ export class HttpResponseFilter {
       const exceptionResponse = exception.getResponse();
 
       if (typeof exceptionResponse === 'string') {
-        // If the response is a string, set it as the message
+        this.logger.verbose(
+          'Exception response is a string, setting as message',
+        );
         errorResponse['message'] = exceptionResponse;
       } else {
-        // If the response is an object, spread it into errorResponse
+        this.logger.verbose(
+          'Exception response is an object, merging with errorResponse',
+        );
         errorResponse = { ...errorResponse, ...exceptionResponse };
       }
     } else {
       errorResponse['message'] = 'Internal Server Error';
       if (process.env.NODE_ENV === 'development') {
-        logger.error(exception);
         errorResponse['error'] = exception.toString();
       }
     }
+
+    this.logger.error(exception);
 
     response.status(status).json(errorResponse);
   }
